b03cb8b8da6b61aa50328614e6a644263d937491
Ryan Missel <rxm7244@rit.edu>
2018-09-05 18:36:19 -0400

    Added batch creation in generate_img, added batch norm to conv1 layer

--- gan_model.py
+++ gan_model.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from scipy import misc
+import numpy as np
 
 # Datasets
 trainset = torchvision.datasets.MNIST(root="./data", train=True,
@@ -25,10 +26,11 @@ class Generator(nn.Module):
         """
         super(Generator, self).__init__()
 
-        self.conv1 = nn.ConvTranspose2d(100, 128, kernel_size=7, stride=2, padding=0)
-        self.conv2 = nn.ConvTranspose2d(128, 56, kernel_size=4, stride=2, padding=1)
-        self.conv3 = nn.ConvTranspose2d(56, 28, kernel_size=4, stride=2, padding=1)
-        self.conv4 = nn.ConvTranspose2d(28, 1, kernel_size=1, stride=1, padding=0)
+        self.conv1 = nn.ConvTranspose2d(100, 128, kernel_size=7, stride=2)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.conv2 = nn.ConvTranspose2d(128, 56, kernel_size=4, stride=2)
+        self.conv3 = nn.ConvTranspose2d(56, 28, kernel_size=4, stride=2)
+        self.conv4 = nn.ConvTranspose2d(28, 1, kernel_size=1, stride=1)
 
     def forward(self, x):
         """
@@ -37,7 +39,7 @@ class Generator(nn.Module):
         :return: Matrix of [-1, 1, 28, 28]
         """
         x = x.view(-1, 100, 1, 1)
-        x = f.leaky_relu(self.conv1(x))
+        x = f.leaky_relu(self.bn1(self.conv1(x)))
         x = f.leaky_relu(self.conv2(x))
         x = f.leaky_relu(self.conv3(x))
         x = f.leaky_relu(self.conv4(x))
@@ -45,30 +47,27 @@ class Generator(nn.Module):
         return x
 
 
-def generate_img(gen, show=False):
+def generate_img(gen, num_images, show=False):
     """
     Simple function that generates a randomized vector of 100 values between [-1,1]
-    and passes it into the generator.
-    :return: A single generated image
+    and passes it into the generator for a specified number of images.
+    :return: A batch of generated images
     """
-    noise = torch.randn(1, 100)
+    noise = torch.rand(num_images, 100)
     image = gen(noise)
-    if show:
+    if show and num_images == 1:
         reshaped = image.view(28, 28).detach()
         misc.toimage(reshaped).show()
     return image
-
-
-generate_img(Generator())
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 128, kernel_size=4, stride=2)
-        self.conv2 = nn.Conv2d(128, 56, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(56, 100, kernel_size=4)
+        self.conv1 = nn.Conv2d(1, 128, kernel_size=6, stride=2)
+        self.conv2 = nn.Conv2d(128, 56, kernel_size=5, stride=2)
+        self.conv3 = nn.Conv2d(56, 100, kernel_size=4, stride=2)
         self.fc = nn.Linear(100, 1)
 
     def forward(self, x):
@@ -77,14 +76,15 @@ class Discriminator(nn.Module):
         x = f.leaky_relu(self.conv3(x))
         x = x.view(-1, 100)
         x = torch.sigmoid(self.fc(x))
-        print(x.size())
         return x
 
 
-image = generate_img(Generator(), True)
-print(image)
+gen = Generator()
+images = generate_img(gen, 50)
+print(images[0:10])
 discrim = Discriminator()
-pred = discrim(image)
+pred = discrim(images)
 print(pred)
+
 
 
